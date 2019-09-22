6a2cc7946549290f547ab2d932217520232e52b2
JukiOK <oky1773@gmail.com>
2019-09-22 13:56:26 -0300

    Passando nos testes, e script aceitando 1 arquivo como argumento

--- function/FuncVisitor.java
+++ function/FuncVisitor.java
@@ -7,162 +7,120 @@ public class FuncVisitor extends FunctionsBaseVisitor<Integer> {
 	private ArrayList<String> var = new ArrayList<String>(); // guarda variáveis
 	private ArrayList<String> param = new ArrayList<String>(); // guarda parametros da função, para verificar se as variáveis estão no escopo dela
 
-//    @Override
-//     public Integer visitRoot(FunctionsParser.RootContext ctx)
-//     {
-//    	System.out.println("Testando como percorrer isso");
-////    	visit(ctx.decVar(0));
-//        return 0;
-//     }
-    // @Override
-    // public Integer visitExprSum(FuncionParser.ExprSumContext ctx)
-    // {
-    //     return visit(ctx.expr(0)) + visit(ctx.expr(1));
-    // }
-    // @Override
-    // public Integer visitExprNum(SummerParser.ExprNumContext ctx)
-    // {
-    //     return Integer.valueOf(ctx.NUM().getText());
-    // }
-
-		//ctx.ID()- pegar contexto de ID, pra navegar na árvore, getText pega valor do contexto
-		//o que mais importa é printar as coisas
-
-//    public Integer visitParenID(FunctionsParser.ParenIDContext ctx) {
-////    	String type = dec.get(ctx.ID().getText());
-////    	if(type != null && type.equals("VAR")) {
-////    		 dec.get(ctx.ID().getText());
-////    	}else if (type == null){
-////			//ERRO não foi declarado
-////			System.out.println("Symbol undeclared: " + ctx.ID().getText());
-////		}else{
-////			//ERRO Bad use
-////			System.out.println("Bad used symbol: " + ctx.ID().getText());
-////		}
-//		return 0;
-//    }
-
-    @Override
-    public Integer visitDecVar(FunctionsParser.DecVarContext ctx) {
-    	if(!var.contains(ctx.ID().getText())) {
-    		var.add(ctx.ID().getText());
-    	}else {
-    	//ERRO já foi declarado
-    		System.out.println("Symbol already declared: " + ctx.ID().getText());
-    	}
-    	System.out.println("No visit decvar teste2: " + var.toString());
+  @Override
+  public Integer visitDecVar(FunctionsParser.DecVarContext ctx) {
+  	if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
+  		var.add(ctx.ID().getText());
+  	}else {
+  		//ERRO já foi declarado
+  		System.out.println("Symbol already declared: " + ctx.ID().getText());
+  	}
+		visit(ctx.expr());
 		return 0;
-    }
+  }
 
 	public Integer visitDecFunc(FunctionsParser.DecFuncContext ctx){
-		param.clear();
-		int n = visit(ctx.ids()); //guardar funções no hash com quantidade de parametros
-		funcs.put(ctx.ID().getText(), n);
-		visit(ctx.expr());
+		if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
+			param.clear();
+			int n = visit(ctx.ids()); //guardar funções no hash com quantidade de parametros
+			funcs.put(ctx.ID().getText(), n);
+			visit(ctx.expr());
+			param.clear();
+		}else{
+			System.out.println("Symbol already declared: " + ctx.ID().getText());
+		}
 		return 0;
 	}
 
 	public Integer visitParamIds(FunctionsParser.ParamIdsContext ctx){
-		System.out.println("visitou paramIds");
 		return visit(ctx.ids(0)) + visit(ctx.ids(1));
 	}
 
 	public Integer visitParamId(FunctionsParser.ParamIdContext ctx){
 		param.add(ctx.ID().getText());
-		System.out.println("visitou paramId");
 		return 1;
 	}
 
-    public Integer visitFunction(FunctionsParser.FunctionContext ctx) {
-     	if(var.contains(ctx.ID().getText())) {
+  public Integer visitFunction(FunctionsParser.FunctionContext ctx) {
+   	if(var.contains(ctx.ID().getText())) {
 		 	//ERRO bad use
 		 	System.out.println("Bad used symbol: " + ctx.ID().getText());
-     	}else if (funcs.get(ctx.ID().getText()) == null){
-		 	//ERRO não foi declarado
-		 	System.out.println("Symbol undeclared: " + ctx.ID().getText());
-     	}
-     	
-     	int n = visit(ctx.values());
-     	
-     	System.out.println("argumentos: " + n);
-     	
+   	}else if (funcs.get(ctx.ID().getText()) == null){
+	 		//ERRO não foi declarado
+	 		System.out.println("Symbol undeclared: " + ctx.ID().getText());
+   	}else{
+			int n = visit(ctx.values());
      	if(n != funcs.get(ctx.ID().getText())) {
      		//ERRO numero de argumentos errado
-     		System.out.println("numero de argumentos errado");
+     		System.out.println("Bad argument count: " + ctx.ID().getText());
      	}
-     	
+		}
 		return 0;
+  }
 
-    }
-    
 
-    public Integer visitExprSoma(FunctionsParser.ExprSomaContext ctx){ 
-        System.out.println("Passou em: visitExprSoma");
-    	visit(ctx.expr());
-    	visit(ctx.muldiv());
-    	return 0;
-    } //visit children vai descer na árvore visitando os filhos
+  public Integer visitExprSoma(FunctionsParser.ExprSomaContext ctx){
+  	visit(ctx.expr());
+  	visit(ctx.muldiv());
+  	return 0;
+  } //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitExprSub(FunctionsParser.ExprSubContext ctx){ 
-        System.out.println("Passou em: visitExprSub");
+	public Integer visitExprSub(FunctionsParser.ExprSubContext ctx){
 		visit(ctx.expr());
-    	visit(ctx.muldiv());
-    	return 0;
+  	visit(ctx.muldiv());
+  	return 0;
 	} //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitExprMuldiv(FunctionsParser.ExprMuldivContext ctx){ 
-        System.out.println("Passou em: visitExprMuldiv");
-		return visit(ctx.muldiv()); 
+	public Integer visitExprMuldiv(FunctionsParser.ExprMuldivContext ctx){
+		return visit(ctx.muldiv());
 	} //visit children vai descer na árvore visitando os filhos
 
 	public Integer visitMuldivMul(FunctionsParser.MuldivMulContext ctx){
-        System.out.println("Passou em: visitMuldivMul");
 		visit(ctx.muldiv());
-    	visit(ctx.paren());
-    	return 0;
+  	visit(ctx.paren());
+  	return 0;
 	} //visit children vai descer na árvore visitando os filhos
 
 	public Integer visitMuldivDiv(FunctionsParser.MuldivDivContext ctx){
-        System.out.println("Passou em: visitMuldivDiv");
-        visit(ctx.muldiv());
-    	visit(ctx.paren());
-    	return 0;
+    visit(ctx.muldiv());
+  	visit(ctx.paren());
+    return 0;
 	} //visit children vai descer na árvore visitando os filhos
 
 	public Integer visitMuldivParen(FunctionsParser.MuldivParenContext ctx){
-        System.out.println("Passou em: visitMuldivParen");
-		return visit(ctx.paren()); 
+		return visit(ctx.paren());
 	} //visit children vai descer na árvore visitando os filhos
 
 	public Integer visitParenID(FunctionsParser.ParenIDContext ctx){
-		System.out.println("parenid");
-		if(!param.contains(ctx.ID().getText()) && !var.contains(ctx.ID().getText())){ //Se não tem id no parametro local, erro não foi declarado
-			System.out.println("Symbol undeclared: " + ctx.ID().getText());
+		if(param.isEmpty() || !param.contains(ctx.ID().getText())){
+			if(funcs.get(ctx.ID().getText()) != null){ // Se já tem uma função com o mesmo nome
+				System.out.println("Bad used symbol: " + ctx.ID().getText());
+			}else if(!var.contains(ctx.ID().getText())){ //Se variável recebe valor de outra variável não declarada
+				System.out.println("Symbol undeclared: " + ctx.ID().getText());
+			}
 		}
-		
 		return 0;
 	}
-	
+
 	public Integer visitParenFunc(FunctionsParser.ParenFuncContext ctx) {
 		return visit(ctx.func());
 	}
-	
+
 	public Integer visitArgvNum(FunctionsParser.ArgvNumContext ctx) {
 		return 1;
 	}
-	
+
 	public Integer visitArgvId(FunctionsParser.ArgvIdContext ctx) {
 		String id = ctx.ID().getText();
 		if(!param.contains(id) && !var.contains(id)) {
 			//ERRO argumento inválido
-			System.out.println("Argumento inválido");
+			System.out.println("Bad used symbol: " + ctx.ID().getText());
 		}
 		return 1;
 	}
-	
+
 	public Integer visitArgvFunc(FunctionsParser.ArgvFuncContext ctx) {
 		return visit(ctx.values(0)) + visit(ctx.values(1));
 	}
 
-//		public Integer visit
 }
--- function/script.sh
+++ function/script.sh
@@ -7,10 +7,10 @@ export CLASSPATH=".:antlr-4.7.2-complete.jar:$CLASSPATH"
 #compiling the .java generated from Functions.g4 with MyParser.java and AddVisitor.java
 javac *.java
 #feeding a string and reading the tokens
-echo "func a(x) 2 + x * 5; func b(d, c)  d + a(c);" | java org.antlr.v4.gui.TestRig Functions root -tokens
+cat $1 | java org.antlr.v4.gui.TestRig Functions root -tokens
 #feeding a string and reading tree in list style
-echo "func a(x) 2 + x * 5; func b(d, c)  d + a(c);" | java org.antlr.v4.gui.TestRig Functions root -tree
+cat $1 | java org.antlr.v4.gui.TestRig Functions root -tree
 #feeding a string and printing a graphical tree
-echo "func a(x) 2 + x * 5; func b(d, c)  d + a(c);" | java org.antlr.v4.gui.TestRig Functions root -gui
+cat $1 | java org.antlr.v4.gui.TestRig Functions root -gui
 #execute the implemented visitor
-echo "func a(x) 2 + x * 5; func b(d, c)  d + a(c);" | java MyParser
+cat $1 | java MyParser
